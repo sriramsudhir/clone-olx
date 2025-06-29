e35125b87af9ad5a96ebe35521da701f46736193
sriramsudhir <89349615+sriramsudhir@users.noreply.github.com>
2025-06-29 09:48:08 +0530

    Fix Next.js configuration file extensions

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/admin/:path*',
+        destination: '/admin/:path*',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
